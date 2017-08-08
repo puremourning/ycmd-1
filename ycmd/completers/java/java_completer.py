13b70ff0d6e7bf62917e56b40dadfcd6b683f810
Ben Jackson <puremourning@gmail.com>
2017-08-08 22:57:14 +0100

    Switch to stdio interface to server; the TCP connection is just fiddly

--- ycmd/completers/java/java_completer.py
+++ ycmd/completers/java/java_completer.py
@@ -111,7 +111,6 @@ class JavaCompleter( language_server_completer.LanguageServerCompleter ):
       self._server = None
       self._server_handle = None
       self._server_stderr = None
-      self._server_stdout = None
 
       self._Reset()
       self._StartServer()
@@ -133,8 +132,8 @@ class JavaCompleter( language_server_completer.LanguageServerCompleter ):
           name = "Java Language Server",
           handle = self._server_handle,
           executable = LANGUAGE_SERVER_HOME,
-          logfiles = [ self._server_stdout, self._server_stderr ],
-          port = [ self._server_stdout_port, self._server_stdin_port ] )
+          logfiles = [ self._server_stderr ],
+        )
       ] )
 
 
@@ -154,15 +153,10 @@ class JavaCompleter( language_server_completer.LanguageServerCompleter ):
       utils.CloseStandardStreams( self._server_handle )
 
     if not self._server_keep_logfiles:
-      if self._server_stdout:
-        utils.RemoveIfExists( self._server_stdout )
-        self._server_stdout = None
       if self._server_stderr:
         utils.RemoveIfExists( self._server_stderr )
         self._server_stderr = None
 
-    self._server_stdin_port = 0
-    self._server_stdout_port = 0
     self._server_handle = None
 
     # TODO: close the sockets in the server
@@ -172,18 +166,6 @@ class JavaCompleter( language_server_completer.LanguageServerCompleter ):
   def _StartServer( self ):
     with self._server_state_mutex:
       _logger.info( 'Starting JDT Language Server...' )
-      self._server_stdin_port = utils.GetUnusedLocalhostPort()
-      self._server_stdout_port = utils.GetUnusedLocalhostPort()
-
-      self._server = language_server_completer.TCPMultiStreamServer(
-        self._server_stdin_port,
-        self._server_stdout_port )
-
-      self._server.start()
-
-      env = os.environ.copy()
-      env[ 'STDIN_PORT' ] = str( self._server_stdin_port )
-      env[ 'STDOUT_PORT' ] = str( self._server_stdout_port )
 
       command = [
         PATH_TO_JAVA,
@@ -205,56 +187,36 @@ class JavaCompleter( language_server_completer.LanguageServerCompleter ):
       _logger.debug( 'Starting java-server with the following command: '
                      '{0}'.format( ' '.join( command ) ) )
 
-      LOGFILE_FORMAT = 'java_language_server_{port}_{std}_'
-
-      self._server_stdout = utils.CreateLogfile(
-          LOGFILE_FORMAT.format( port = self._server_stdout_port,
-                                 std = 'stdout' ) )
+      LOGFILE_FORMAT = 'java_language_server_{pid}_{std}_'
 
       self._server_stderr = utils.CreateLogfile(
-          LOGFILE_FORMAT.format( port = self._server_stdin_port,
-                                 std = 'stderr' ) )
+          LOGFILE_FORMAT.format( pid = os.getpid(), std = 'stderr' ) )
 
-      with utils.OpenForStdHandle( self._server_stdout ) as stdout:
-        with utils.OpenForStdHandle( self._server_stderr ) as stderr:
-          self._server_handle = utils.SafePopen( command,
-                                                 stdin = PIPE,
-                                                 stdout = stdout,
-                                                 stderr = stderr,
-                                                 env = env )
+      with utils.OpenForStdHandle( self._server_stderr ) as stderr:
+        self._server_handle = utils.SafePopen( command,
+                                               stdin = PIPE,
+                                               stdout = PIPE,
+                                               stderr = stderr )
 
       if self._ServerIsRunning():
-        _logger.info( 'JDT Language Server started, '
-                      'with stdin {0}, stdout {1}'.format(
-                        self._server_stdin_port,
-                        self._server_stdout_port ) )
+        _logger.info( 'JDT Language Server started' )
       else:
         _logger.warning( 'JDT Language Server failed to start' )
         return
+
+      self._server = (
+        language_server_completer.StandardIOLanguageServerConnection(
+          self._server_handle.stdin,
+          self._server_handle.stdout )
+      )
+
+      self._server.start()
 
       # Awaiting connection
       # spinlock
       # TODO: timeout
       self._server.TryServerConnection()
 
-    # OK, so now we have to fire the Initialise request to the server:
-    #
-    # LS PROTOCOL - Initialise request
-    #
-    # The initialize request is sent as the first request from the client to
-    # the server. If the server receives request or notification before the
-    # initialize request it should act as follows:
-    # - for a request the respond should be errored with code: -32001. The
-    #   message can be picked by the server.
-    # - notifications should be dropped.
-    #
-    # TODO/FIXME: This causes a hang on startup waiting for the
-    # server. I suspect this is holding the server state mutex, and any
-    # requests that come in (such as OnFileReadyToParse) just get blocked
-    # waiting for this to happen. The fix is to have a proper state model, or
-    # at least a simple one, rather than leaning on a lock (assuming that is
-    # the actual problem)
-    #
     self._WaitForInitiliase()
 
 
