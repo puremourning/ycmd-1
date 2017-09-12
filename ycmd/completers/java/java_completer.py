113bddd40fa32f04fde3c638f5614562ee7ff19c
Ben Jackson <puremourning@gmail.com>
2017-09-12 18:05:14 +0100

    Add more info to the debug output

--- ycmd/completers/java/java_completer.py
+++ ycmd/completers/java/java_completer.py
@@ -141,8 +141,13 @@ class JavaCompleter( language_server_completer.LanguageServerCompleter ):
           logfiles = [
             self._server_stderr,
             os.path.join( self._workspace_path, '.metadata', '.log' )
-          ],
-        )
+          ] )
+      ],
+      items = [
+        responses.DebugInfoItem( 'Workspace Path', self._workspace_path ),
+        responses.DebugInfoItem( 'Java Path', PATH_TO_JAVA ),
+        responses.DebugInfoItem( 'jdt.ls Path', _PathToLauncherJar() ),
+        responses.DebugInfoItem( 'Launcher Config.', _LauncherConfiguration() ),
       ] )
 
 
