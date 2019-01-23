0defe4f55082e5317adba9e30747372a279e94d9
Ben Jackson <puremourning@gmail.com>
2019-01-23 22:35:49 +0000

    WIP: For fixit-completions, include an indicator in the message to the clent. This allows the client to set the cursor position more sensibly, or at least in keeping with what vscode does

--- ycmd/responses.py
+++ ycmd/responses.py
@@ -175,11 +175,12 @@ class FixIt( object ):
   must be byte offsets into the UTF-8 encoded version of the appropriate
   buffer.
   """
-  def __init__( self, location, chunks, text = '' ):
+  def __init__( self, location, chunks, text = '', is_completion = False ):
     """location of type Location, chunks of type list<FixItChunk>"""
     self.location = location
     self.chunks = chunks
     self.text = text
+    self.is_completion = is_completion
 
 
 class FixItChunk( object ):
@@ -271,6 +272,7 @@ def BuildFixItResponse( fixits ):
       'location': BuildLocationData( fixit.location ),
       'chunks' : [ BuildFixitChunkData( x ) for x in fixit.chunks ],
       'text': fixit.text,
+      'is_completion': fixit.is_completion,
     }
 
   return {
