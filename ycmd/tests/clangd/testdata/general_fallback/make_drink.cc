07d25f44f2c9f4d1862de60b2f9bc79ad6919be2
Ben Jackson <puremourning@gmail.com>
2019-05-31 23:26:30 +0100

    Add an annoying test

--- ycmd/tests/clangd/testdata/general_fallback/make_drink.cc
+++ ycmd/tests/clangd/testdata/general_fallback/make_drink.cc
@@ -2,8 +2,24 @@
 
 using namespace Test;
 
+struct Testing {
+  int a_really_really_really_really_annoyingly_long_method_name(
+    int with,
+    char a,
+    bool lot,
+    const char * of,
+    Testing& arguments
+  );
+  int a_really_really_really_really_annoyingly_long_method_name(
+    int with_only_one_really_really_really_annoyingly_long_argument
+  );
+};
+
 int main( int , char ** )
 {
   make_drink( TypeOfDrink::COFFEE, 10.0, Flavour::ELDERFLOWER );
   make_drink( TypeOfDrink::JUICE, Temperature::COLD, 1 );
+
+  Testing t;
+  t.a_really_really_really_really_annoyingly_long_method_name( 
 }
