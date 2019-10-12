ea9f43257148c30d2fb5b7d12c3431b2672df01f
Boris Staletic <boris.staletic@gmail.com>
2019-10-12 19:54:20 +0200

    Add API documentation

--- ycmd/tests/language_server/generic_completer_test.py
+++ ycmd/tests/language_server/generic_completer_test.py
@@ -252,7 +252,6 @@ def GenericLSPCompleter_DebugInfo_CustomRoot_test( app, *args ):
       'filetypes': [ 'foo' ],
       'project_root_files': [ 'proj_root' ],
       'cmdline': [ 'node', PATH_TO_GENERIC_COMPLETER, '--stdio' ] } ] } )
-<<<<<<< HEAD
 def GenericLSPCompleter_SignatureHelp_NoTriggers_test( app ):
   test_file = PathToTestFile(
       'generic_server', 'foo', 'bar', 'baz', 'test_file' )
@@ -275,7 +274,8 @@ def GenericLSPCompleter_SignatureHelp_NoTriggers_test( app ):
     } ),
     'errors': empty()
   } ) )
-=======
+
+
 def GenericLSPCompleter_SignatureHelp_NotSupported_test( app ):
   test_file = PathToTestFile(
       'generic_server', 'foo', 'bar', 'baz', 'test_file' )
@@ -291,4 +291,3 @@ def GenericLSPCompleter_SignatureHelp_NotSupported_test( app ):
   response = app.get( '/signature_help_available',
                       { 'subserver': 'foo' } ).json
   assert_that( response, SignatureAvailableMatcher( 'NO' ) )
->>>>>>> Provide a way for clients to query if signature help is available
